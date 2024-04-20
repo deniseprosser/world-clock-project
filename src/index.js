b341b929b80dfccc42c80e3a44ef18f5c7196573
deniseprosser <denise.prosser23@gmail.com>
2024-04-20 17:29:03 +0100

    Added JS for single location times/dates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let vancouverElement = document.querySelector("#vancouver");
+  let vancouverDateElement = vancouverElement.querySelector(".date");
+  let vancouverTimeElement = vancouverElement.querySelector(".time");
+  let vancouverTime = moment().tz("America/Vancouver");
 
+  vancouverDateElement.innerHTML = vancouverTime.format("Do MMMM YYYY");
+  vancouverTimeElement.innerHTML = vancouverTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("Do MMMM YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1);
